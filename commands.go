89d8ccf8d609313339823196db813aee6b83596f
Alexey Zagalsky <alexeyza@gmail.com>
2017-03-04 14:13:31 -0800

    remove duplicate code

--- commands.go
+++ commands.go
@@ -16,17 +16,17 @@ func respond(res http.ResponseWriter, obj interface{}) {
 func handleStart(res http.ResponseWriter, req *http.Request) {
 	//data, err := NewGameStartRequest(req)
 	_, err := NewGameStartRequest(req)
-	if err != nil {
-		respond(res, GameStartResponse{
-			Taunt:          toStringPointer("Me eat brains..."),
-			Color:          "#f26000",
-			Name:           "ZombieSnake", //fmt.Sprintf("%v (%vx%v)", data.GameId, data.Width, data.Height),
-			HeadUrl:        toStringPointer(fmt.Sprintf("%v://%v/head.png")),
-			HeadType:       "pixel",
-			TailType:       "tail_type",
-			SecondaryColor: "#ffffff",
-		})
-	}
+	// if err != nil {
+	// 	respond(res, GameStartResponse{
+	// 		Taunt:          toStringPointer("Me eat brains..."),
+	// 		Color:          "#f26000",
+	// 		Name:           "ZombieSnake", //fmt.Sprintf("%v (%vx%v)", data.GameId, data.Width, data.Height),
+	// 		HeadUrl:        toStringPointer(fmt.Sprintf("%v://%v/head.png")),
+	// 		HeadType:       "pixel",
+	// 		TailType:       "tail_type",
+	// 		SecondaryColor: "#ffffff",
+	// 	})
+	// }
 
 	scheme := "http"
 	if req.TLS != nil {
@@ -46,13 +46,13 @@ func handleStart(res http.ResponseWriter, req *http.Request) {
 func handleMove(res http.ResponseWriter, req *http.Request) {
 	//data, err := NewMoveRequest(req)
 	_, err := NewMoveRequest(req)
-	if err != nil {
-		respond(res, MoveResponse{
-			Move:  "up",
-			Taunt: toStringPointer("You can't hide from me..."),
-		})
-		return
-	}
+	// if err != nil {
+	// 	respond(res, MoveResponse{
+	// 		Move:  "up",
+	// 		Taunt: toStringPointer("You can't hide from me..."),
+	// 	})
+	// 	return
+	// }
 
 	directions := []string{
 		"up",
